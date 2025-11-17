06f5f74f44fc047143726b53f478c22af5e5245c
Dimaikel <dimaikelsantiagointu@gmail.com>
2025-11-17 15:38:36 -0400

    estructura final

--- sistema_gestion/settings.py
+++
@@ -1,7 +0,0 @@
-# Configuración de login
-LOGIN_REDIRECT_URL = '/clientes/'  # Donde va después de login
-LOGOUT_REDIRECT_URL = '/accounts/login/'  # Donde va después de logout
-LOGIN_URL = '/accounts/login/'
-
-# Para desarrollo - quitar en producción
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
